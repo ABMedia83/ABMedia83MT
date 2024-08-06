7e05b1bcf01a40f14c701a8863423f4abfc5946a
ABMedia83 <amxp2083@gmail.com>
2024-08-06 17:47:37 -0400

    all

---
+++ Albert.Win32/Items/ColorItem.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Albert.Win32.Items;
+
+/// <summary>
+/// Class is desnigned to store a WPF Color
+/// </summary>
+public sealed class ColorItem : Item
+{
+	public ColorItem()
+	{
+		Color = HexColor("#0000000");
+	}
+	public ColorItem(string _hex)
+	{
+		Color = HexColor(_hex);
+	}
+
+	public ColorItem(Color _color)
+	{
+		Color = _color;
+	}
+
+	public ColorItem(byte _alpha, byte _red, byte _green, byte _blue)
+	{
+		Color = Color.FromArgb(_alpha, _red, _green, _blue);
+	}
+
+	public ColorItem(byte _red, byte _green, byte _blue)
+	{
+		Color = Color.FromRgb(_red, _green, _blue);
+	}
+
+	public Color Color { get; init; }
+
+
+	public override string ToString()
+	{
+		return $"{Name}:{Color}";
+	}
+
+}
+
---
+++ Albert.Win32/Items/FontItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Albert.Win32.Items
+{
+    /// <summary>
+    /// This class is used to store FontFamily in an itmes type list.
+    /// </summary>
+    public sealed class FontItem: Item
+    {
+        public FontItem(string _name)
+        {
+            Name = _name;
+            FontFamily = new FontFamily(_name);
+        }
+
+        
+        public FontFamily FontFamily {get;init;}
+
+        public override string ToString()
+        {
+            return Name!;
+        }
+    }
+}
